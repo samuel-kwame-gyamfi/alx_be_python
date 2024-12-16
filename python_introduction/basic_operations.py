bb5b1694e9429c4032f592caa5f2b1a58793d016
samuel <samuelkwameowusugyamfi@gmail.com>
2024-12-16 03:33:15 -0800

    basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,16 @@
+#basic operations
+#delaring various variables
+number1 = 10
+number2 = 5
+
+#performing the arithmetic operations
+ addition_result = number1 + number2
+ subtraction_result = number1 - number2
+ multiplication_result = number1 * number2
+
+ #printing out the output  
+
+ print(f"[Addition] of [{number1}] and [{number2}] is [{addition_result}]")
+print(f"[Subtraction] of [{number1}] and [{number2}] is [{subtraction_result}]")
+print(f"[Multiplication] of [{number1}] and [{number2}] is [{multiplication_result}]")
+
