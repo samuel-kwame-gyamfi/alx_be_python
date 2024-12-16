af09bc8f19a698ea5f7f21cc2a48d0940fd38618
samuel <samuelkwameowusugyamfi@gmail.com>
2024-12-16 07:41:26 -0800

    age in 2050
    exit

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,10 @@
+#calculating the age of a user
+#input a prompt
+
+current_age = int(input("How old are you?"))
+
+#age in 2050
+age_in_2050 = current_age + 27
+#printing the output
+print(f"In 2050, you will be {age_in_2050} years old")
+
