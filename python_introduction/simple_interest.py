469fb938ec7d261717fc873639b4e7a00f838f2f
samuel <samuelkwameowusugyamfi@gmail.com>
2024-12-16 07:04:16 -0800

    simple_interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,14 @@
+#Simple interest calculator
+#Declaring all the variables
+Principal = 1000
+Rate = 0.05
+Time = 3
+
+#Calculation code
+
+Interest = Principal * Rate * Time
+
+#Printing the output
+
+print(f"The simple interest is: {Interest}")
+
