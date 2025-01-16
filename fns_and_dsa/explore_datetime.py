a1d95c76b3db8318a85521ea1f123ae27a7eb4c5
samuel-kwame-gyamfi <samuelkwameowusugyamfi@gmail.com>
2025-01-16 20:39:32 +0000

    date and time

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,17 @@
+from datetime import datetime, timedelta
+
+def display_current_datetime():
+    current_date = datetime.now()
+    formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
+    print(f"Current Date and Time: {formatted_date}")
+
+def calculate_future_date():
+    days_to_add = int(input("Enter number of days to add: "))
+    current_date = datetime.now()
+    future_date = current_date + timedelta(days=days_to_add)
+    formatted_future_date = future_date.strftime("%Y-%m-%d")
+    print(f"Future Date: {formatted_future_date}")
+
+if __name__ == "__main__":
+    display_current_datetime()
+    calculate_future_date()
